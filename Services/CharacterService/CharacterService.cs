46b58793d1b32cf149e4eb7187fc6dc20f54f61b
rigobertovillanueva <rigo.villanueva@gmail.com>
2023-06-10 17:20:10 -0400

    Commit 17:20

---
+++ Services/CharacterService/CharacterService.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_rpg.Services.CharacterService
+{
+    public class CharacterService : ICharacterService
+    {
+        public List<Character> AddCharacter(Character newCharacter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Character> GetAllCharacters()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Character GetCharacterById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
