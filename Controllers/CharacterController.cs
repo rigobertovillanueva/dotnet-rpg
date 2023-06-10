d5ad2635080e61bff22e04a30005ba25f0023506
rigobertovillanueva <rigo.villanueva@gmail.com>
2023-06-10 16:40:41 -0400

    Commit 16:40

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -13,13 +13,39 @@ namespace dotnet_rpg.Controllers
     public class CharacterController : ControllerBase
     {
 
-        private static Character knight = new Character();
-        [HttpGet]
-        public ActionResult<Character> Get()
+        private static List<Character> characters = new List<Character> {
+            new Character(),
+            new Character {Id = 1, Name = "Sam"}
+        };
+
+        [HttpGet("GetAll")]
+        public ActionResult<List<Character>> Get()
         {
-
-            return Ok(knight);
-
+            return Ok(characters);
         }
+
+        [HttpGet("{id}")]
+        public ActionResult<Character> GetSingle(int id)
+        {
+            return Ok(characters.FirstOrDefault(c => c.Id == id));
+        }
+
+
+        [HttpPost]
+        public ActionResult<List<Character>> AddCharacter(Character newCharacter)
+        {
+            characters.Add(newCharacter);
+            return Ok(characters);
+        }
+
+
+
+
+        /* [HttpGet]
+         public ActionResult<Character> Get()
+         {
+             return Ok(knight);
+         }*/
+
     }
 }
