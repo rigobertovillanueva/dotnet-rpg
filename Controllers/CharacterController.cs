96490b47078a35be181aab4fd5cd949c42086c0f
rigobertovillanueva <rigo.villanueva@gmail.com>
2023-06-21 16:28:16 -0400

    Commit 16:28

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -46,9 +46,23 @@ namespace dotnet_rpg.Controllers
         {
             var response = await _characterService.UpdateCharacter(updatedCharacter);
             if (response.Data is null)
-            { return NotFound(response); }
+            {
+                return NotFound(response);
+            }
             return Ok(response);
         }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<ServiceResponse<List<GetCharacterDto>>>> DeleteCharacter(int id)
+        {
+            var response = await _characterService.DeleteCharacter(id);
+            if (response.Data is null)
+            {
+                return NotFound(response);
+            }
+            return Ok(response);
+        }
+
 
     }
 }
